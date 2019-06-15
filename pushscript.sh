437f793d7ea3d6f0d33d6a2502050618ae38a898
ritvik06 <rvij@DESKTOP-1TL12UE.localdomain>
2019-06-15 15:41:47 +0530

    message

--- pushscript.sh
+++ pushscript.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
+git pull origin master
 git add .
+read -p "Enter your commit message: " message
 git config user.name 'ritvik06'
-git commit -m 'one updated file'
+git commit -m message
 git push origin master
-
-#expand this script for prompts, add certain files or the default add ., add prompt for commiting the message, add prompt for setting the remote, taking details input for 
-#refs etc.
-
-#make automated script for the carla environment for the model to train and then the model to load in the client_example.py and then give the controls to the server,give prompts for pull, use force merge in cases of prompt(have to look into ways for merging conflits or more like removing them)
-
-#testing
